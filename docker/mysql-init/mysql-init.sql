99509390a33f69e9788f1c048aba3e16318556e3
ryanluu12345 <ryanluu12345@gmail.com>
2023-11-07 09:00:58 -0500

    docker: added in logic to seed the DB within the container with starter data (#49)
    Bulk insert data for the mysql and pg source data so that it's easier to test.
    
    Release Note: None

--- docker/mysql-init/mysql-init.sql
+++ docker/mysql-init/mysql-init.sql
@@ -10,6 +10,32 @@ CREATE TABLE employees (
     salary DECIMAL(8, 2),
     bonus FLOAT
 );
-INSERT INTO employees (unique_id, name, created_at, updated_at, is_hired, salary, bonus)
-VALUES (UNHEX(REPLACE('550e8400-e29b-41d4-a716-446655440000', '-', '')), 'John Doe', '2023-11-03 09:00:00', '2023-11-03', 1, 5000.00, 100.25);
+
+DELIMITER $$
+CREATE PROCEDURE InsertEmployeesWithTransaction()
+BEGIN
+    DECLARE i INT;
+    SET i = 1;
+    
+    START TRANSACTION;
+
+    WHILE i <= 200000 DO
+        INSERT INTO employees (unique_id, name, created_at, updated_at, is_hired, salary, bonus)
+        VALUES (
+            UNHEX(REPLACE('550e8400-e29b-41d4-a716-446655440000', '-', '')),
+            CONCAT('Employee_', i),
+            '2023-11-03 09:00:00',
+            '2023-11-03',
+            1,
+            5000.00,
+            100.25
+        );
+        SET i = i + 1;
+    END WHILE;
+
+    COMMIT;
+END$$
+DELIMITER ;
+
+CALL InsertEmployeesWithTransaction();
 
--- docker/pg-init/pg-init.sql
+++ docker/pg-init/pg-init.sql
@@ -12,13 +12,22 @@ CREATE TABLE employees (
     bonus REAL
 );
 
-INSERT INTO employees (unique_id, name, created_at, updated_at, is_hired, salary, bonus)
-VALUES (
-    '550e8400-e29b-41d4-a716-446655440000'::uuid,
-    'John Doe',
-    '2023-11-03 09:00:00'::timestamp,
-    '2023-11-03'::date,
-    true,
-    5000.00,
-    100.25
-);
+DO $$ 
+DECLARE 
+    i INT;
+BEGIN
+    i := 1;
+    WHILE i <= 200000 LOOP
+        INSERT INTO employees (unique_id, name, created_at, updated_at, is_hired, salary, bonus)
+        VALUES (
+            ('550e8400-e29b-41d4-a716-446655440000'::uuid),
+            'Employee_' || i,
+            '2023-11-03 09:00:00'::timestamp,
+            '2023-11-03'::date,
+            true,
+            5000.00,
+            100.25
+        );
+        i := i + 1;
+    END LOOP;
+END $$;
